56f84fc3f83d2ebbbec9d030e28b3a50a1b91ab8
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-10-28 13:23:10 +0300

    Test2

--- server/server.js
+++ server/server.js
@@ -27,12 +27,14 @@ app.use (express.json ());
 app.use (
   cors ({
     origin: process.env.NODE_ENV === 'production'
-      ? 'https://graphfusion-coding-test-1.onrender.com' // Your frontend Render URL
-      : 'http://localhost:5173', // Vite's default development port
-    credentials: true, // If you're using cookies/sessions
+      ? 'https://graphfusion-coding-test-1.onrender.com'
+      : 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
 // Routes
 app.use (nodeRoute);
 app.use (relationshipRoute);
